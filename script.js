7b067d79e1930e37e773e63c9fb8e0f0f94cb417
Marcos Vinicius <mvsouza7@gmail.com>
2021-05-07 15:46:13 +0300

    applying the keydown event connected with the piano keyboards

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Get all the keys
-
 let keys = document.querySelectorAll(".key");
-console.log(keys)
+// console.log(keys)
+
 
 //Play the notes
+function playNote(event) {
+console.log(event)
+
+}
+
+
+
+
+//Keyboard type
+window.addEventListener("keydown", playNote)
