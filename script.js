22701b4c919ba976e0e5c97df53dfea1dc892503
Marcos Vinicius <mvsouza7@gmail.com>
2021-05-07 16:50:13 +0300

    Testing variables and functions

--- script.js
+++ script.js
@@ -5,12 +5,53 @@ let keys = document.querySelectorAll(".key");
 
 //Play the notes
 function playNote(event) {
-console.log(event)
+    // To see on the console which events
+    // console.log(event)
 
+    // To see on the console which keycodes numbers when I pressed the pc keyboard buttons.
+    // console.log(event.keyCode)
+
+    // To see on the console which data-key(HTML) i'm clicking
+    // console.log(event.target.dataset.key)
+
+
+    // keyCode reference
+   let audioKeyCode = getKeyCode(event);
+
+
+    // console.log(event.type)
+    // console.log(keyCode)
+    // one console.log to show the representative event when pressed or clicked
+    // one console.log and other to show if it appears when clicked or pressed.
+
+
+    // Typed or pressed key
+    
+
+    // If key exists 
+
+
+    // Play the respective sound of each key pressed
 }
 
 
+function getKeyCode(event) {
+    let keyCode;
+
+    let isKeyboard = event.type === "keydown" //true or false (boolean)
+    if (isKeyboard) {
+        keyCode = event.keyCode
+    } else {
+        keyCode = event.target.dataset.key
+    }
+}
 
 
-//Keyboard type
+//Click events with mouse
+// window.addEventListener("click", playNote)
+keys.forEach( function(key){
+    key.addEventListener("click", playNote);
+})
+
+//Keyboard events
 window.addEventListener("keydown", playNote)
